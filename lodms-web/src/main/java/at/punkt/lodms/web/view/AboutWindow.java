f600c49d27fd5f385856dcaa7a6d25575a5a6ee4
nagyhel <h.nagy@semantic-web.at>
2013-04-11 11:35:18 +0200

    changed info and licence

--- lodms-web/src/main/java/at/punkt/lodms/web/view/AboutWindow.java
+++ lodms-web/src/main/java/at/punkt/lodms/web/view/AboutWindow.java
@@ -21,13 +21,13 @@ public class AboutWindow extends Window {
 
     public AboutWindow() {
         super("About LOD Management Suite");
-        Label aboutText = new Label("<div class=\"lodms_about\"><p>The Linked (Open) Data Manager is developed by the <a href=\"http://www.semantic-web.at\">Semantic Web Company</a> in the course of the <a href=\"http://lod2.eu\">LOD2</a> FP7 project.</p>" +
+        Label aboutText = new Label("<div class=\"lodms_about\"><p>The Linked (Open) Data Management Suite is developed by the <a href=\"http://www.semantic-web.at\">Semantic Web Company</a> in the course of the <a href=\"http://lod2.eu\">LOD2</a> FP7 project.</p>" +
                                     "<p>It is a Java based Linked (Open) Data Management Suite to schedule and monitor required ETL: Extract - Transform - Load</br>jobs for smooth and efficient Linked (Open) Data Management for web-based Linked Open Data portals (LOD platforms)</br>as well as for sustainable Data Management and Data Integration usage inside of the enterprise / the organisation.</p>" +
                                     "<ul><li>Main Developer: Alexander Kreiser (<a href=\"mailto:akreiser@gmail.com\">akreiser@gmail.com</a>)</li>" +
                                     "<li>Contact: <a href=\"mailto:support@poolparty.biz\">support@poolparty.biz</a></li>" +
                                     "<li>Documentation: <a href=\"https://grips.semantic-web.at/display/LDM\">LOD Management Suite Documentation (LODMS)</a></li>" +
-                                    "<li>Google Code: <a href=\"http://code.google.com/p/lod-ms/\">http://code.google.com/p/lod-ms/</a></li></ul>" +
-                                    "<p>Release 1.0, <a href=\"http://www.apache.org/licenses/LICENSE-2.0\">Apache Licence 2.0</a></div>",
+                                    "<li>Github: <a href=\"https://github.com/lodms\">https://github.com/lodms</a></li></ul>" +
+                                    "<p>Release 1.0, <a href=\"http://www.gnu.org/licenses/gpl-2.0.html\">GPLv2</a></div>",
                                     Label.CONTENT_XHTML);
         VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
