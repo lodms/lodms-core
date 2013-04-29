7f7844db601684900f643458e1a25f0e57d92788
nielsv <niels.vandekeybus@tenforce.com>
2013-04-29 14:53:40 +0200

    added necessary no arg constructors/setters for xstream

--- lodms-web/src/main/java/at/punkt/lodms/web/persistence/ComponentBlueprint.java
+++ lodms-web/src/main/java/at/punkt/lodms/web/persistence/ComponentBlueprint.java
@@ -5,33 +5,40 @@
 package at.punkt.lodms.web.persistence;
 
 /**
- *
  * @author Alex Kreiser
  */
 public class ComponentBlueprint<T> {
-    
-    private Class<? extends T> type;
-    private Object config;
-    private boolean disabled;
 
-    public ComponentBlueprint(Class<? extends T> type, Object config) {
-        this.type = type;
-        this.config = config;
-    }
-    
-    public Object getConfig() {
-        return config;
-    }
+  private Class<? extends T> type;
+  private Object config;
+  private boolean disabled;
 
-    public Class<? extends T> getType() {
-        return type;
-    }
+  public ComponentBlueprint() {
 
-    public boolean isDisabled() {
-        return disabled;
-    }
+  }
 
-    public void setDisabled(boolean disabled) {
-        this.disabled = disabled;
-    }
+  public ComponentBlueprint(Class<? extends T> type, Object config) {
+    this.type = type;
+    this.config = config;
+  }
+
+  public void setConfig(Object config) {
+    this.config = config;
+  }
+
+  public Object getConfig() {
+    return config;
+  }
+
+  public Class<? extends T> getType() {
+    return type;
+  }
+
+  public boolean isDisabled() {
+    return disabled;
+  }
+
+  public void setDisabled(boolean disabled) {
+    this.disabled = disabled;
+  }
 }
